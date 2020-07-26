88641ec398f3c0124f27a190c15b0bed38a083f5
Seb <seb.arboleda@outlook.com>
2020-07-26 19:55:04 -0400

    Add slider intro

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        document.getElementById("preLoader").classList.add('height-0');
+        console.log("FIRED!!!")
+    }, 2000);
+});
